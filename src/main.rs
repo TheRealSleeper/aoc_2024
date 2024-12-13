f221ace9a3ef7b183f44fcc7906c2133776e787f
TheRealSleeper <140853156+TheRealSleeper@users.noreply.github.com>
2024-12-12 18:52:06 -0600

    changed master to act as template

--- src/main.rs
+++ src/main.rs
@@ -36,81 +36,12 @@ fn main() {
     }
 }
 
-const STEP_MAX: u8 = 3;
-const STEP_MIN: u8 = 1;
-
-#[derive(Debug, PartialEq, Clone, Copy)]
-enum GroupType {
-    Undecided,
-    Up,
-    Down,
-}
 
 fn part1(input: &str) -> isize {
-    input
-        .lines()
-        .map(|l| {
-            l.split_ascii_whitespace()
-                .map(|w| w.parse::<u8>().unwrap())
-                .collect::<Vec<u8>>()
-        })
-        .filter(check_group)
-        .count() as isize
+    todo!()
 }
+
 
 fn part2(input: &str) -> isize {
-    input
-        .lines()
-        .map(|l| {
-            l.split_ascii_whitespace()
-                .map(|w| w.parse::<u8>().unwrap())
-                .collect::<Vec<u8>>()
-        })
-        .filter(|g| {
-            if !check_group(g) {
-                for i in 0..g.len() {
-                    let mut tmp = g.clone(); 
-                    tmp.remove(i); 
-                    if check_group(&tmp) {
-                        return true;
-                    }
-                }
-
-                return false;
-            } else {
-                true
-            }
-        })
-        .count() as isize
-}
-
-fn check_group(inp: &Vec<u8>) -> bool {
-    let mut state = GroupType::Undecided;
-    for w in inp.windows(2) {
-        match state {
-            GroupType::Undecided => {
-                if w[0] < w[1] && w[0].abs_diff(w[1]) >= STEP_MIN && w[0].abs_diff(w[1]) <= STEP_MAX {
-                    state = GroupType::Up;
-                } else if w[0] > w[1] && w[0].abs_diff(w[1]) >= STEP_MIN && w[0].abs_diff(w[1]) <= STEP_MAX {
-                    state = GroupType::Down;
-                } else {
-                    return false;
-                }
-            }
-
-            GroupType::Up => {
-                if w[0] > w[1] || w[0].abs_diff(w[1]) < STEP_MIN || w[0].abs_diff(w[1]) > STEP_MAX {
-                    return false;
-                }
-            }
-
-            GroupType::Down => {
-                if w[0] < w[1] || w[0].abs_diff(w[1]) < STEP_MIN || w[0].abs_diff(w[1]) > STEP_MAX {
-                    return false;
-                }
-            }
-        }
-    }
-
-    return true;
+    todo!()
 }
