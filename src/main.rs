3f403fa882be66b58b14ca361741b2540118dc45
TheRealSleeper <140853156+TheRealSleeper@users.noreply.github.com>
2024-12-16 12:46:01 -0600

    solved part 1

--- src/main.rs
+++ src/main.rs
@@ -37,8 +37,15 @@ fn main() {
 }
 
 fn search_all_dir(map: &Vec<Vec<char>>, target_row: usize, target_col: usize, accum: &mut isize) {
-    for r in target_row.saturating_sub(1)..(target_row + 1).clamp(0, map.len() - 1) {
-        for c in target_col.saturating_sub(1)..(target_col + 1).clamp(0, map[r].len() - 1) {
+    for r in target_row.saturating_sub(1)..=(target_row + 1).clamp(0, map.len() - 1) {
+        for c in target_col.saturating_sub(1)..=(target_col + 1).clamp(0, map[r].len() - 1) {
+            // println!(
+            //     "Starting search at row {}, column {}, direction {}, {}",
+            //     target_row + 1,
+            //     target_col + 1,
+            //     r as isize - target_row as isize,
+            //     c as isize - target_col as isize,
+            // );
             search_directional(
                 map,
                 r,
@@ -61,18 +68,18 @@ fn search_directional(
     target_char: char,
     accum: &mut isize,
 ) {
-    let current_char = map[target_row][target_col]; 
-    // println!("Current character is {} at row {}, column {}", 
-    //     current_char, 
-    //     target_row + 1, 
-    //     target_col + 1); 
+    let current_char = map[target_row][target_col];
+    // println!("Current character is {} at row {}, column {}",
+    //     current_char,
+    //     target_row + 1,
+    //     target_col + 1);
     if target_char == current_char {
-        println!(
-            "\'{}\' at row {}, column {}",
-            target_char,
-            target_row + 1,
-            target_col + 1
-        );
+        // println!(
+        //     "\'{}\' at row {}, column {}",
+        //     target_char,
+        //     target_row + 1,
+        //     target_col + 1
+        // );
         match target_char {
             'M' => {
                 if target_row as isize + (dir_row as isize) >= 0
@@ -83,7 +90,7 @@ fn search_directional(
                     search_directional(
                         map,
                         target_row.saturating_add_signed(dir_row),
-                        target_col.saturating_add_signed(dir_col), 
+                        target_col.saturating_add_signed(dir_col),
                         dir_row,
                         dir_col,
                         'A',
@@ -99,9 +106,9 @@ fn search_directional(
                     && target_col as isize + (dir_col as isize) < map[target_row].len() as isize
                 {
                     search_directional(
-                        map, 
+                        map,
                         target_row.saturating_add_signed(dir_row),
-                        target_col.saturating_add_signed(dir_col), 
+                        target_col.saturating_add_signed(dir_col),
                         dir_row,
                         dir_col,
                         'S',
@@ -119,6 +126,7 @@ fn search_directional(
     }
 }
 
+#[allow(unused_variables)]
 fn part1(input: &str) -> isize {
     let map = input
         .lines()
@@ -129,7 +137,8 @@ fn part1(input: &str) -> isize {
     for row in 0..map.len() {
         for column in 0..map[row].len() {
             if map[row][column] == 'X' {
-                println!("\'X\' at row {}, column {}", row + 1, column + 1);
+                // println!("Starting search at row {}, column {}", row + 1, column + 1);
+                // println!("Starting at \'X\' at row {}, column {}", row + 1, column + 1);
                 search_all_dir(&map, row, column, &mut count);
             }
         }
@@ -138,6 +147,7 @@ fn part1(input: &str) -> isize {
     return count;
 }
 
+#[allow(unused_variables)]
 fn part2(input: &str) -> isize {
     todo!()
 }
