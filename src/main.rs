e7b135e6080ba6155e95e93b0d0a7186358fb6b4
TheRealSleeper <140853156+TheRealSleeper@users.noreply.github.com>
2024-12-20 11:08:57 -0600

    reverted to correct day 3 code instead of day 4

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::{fs::read_to_string, ops::AddAssign};
+use regex::RegexBuilder;
+use std::fs::read_to_string;
 
 mod aoc_utils;
 mod tests;
@@ -36,118 +37,32 @@ fn main() {
     }
 }
 
-fn search_all_dir(map: &Vec<Vec<char>>, target_row: usize, target_col: usize, accum: &mut isize) {
-    for r in target_row.saturating_sub(1)..=(target_row + 1).clamp(0, map.len() - 1) {
-        for c in target_col.saturating_sub(1)..=(target_col + 1).clamp(0, map[r].len() - 1) {
-            // println!(
-            //     "Starting search at row {}, column {}, direction {}, {}",
-            //     target_row + 1,
-            //     target_col + 1,
-            //     r as isize - target_row as isize,
-            //     c as isize - target_col as isize,
-            // );
-            search_directional(
-                map,
-                r,
-                c,
-                r as isize - target_row as isize,
-                c as isize - target_col as isize,
-                'M',
-                accum,
-            );
-        }
-    }
-}
-
-fn search_directional(
-    map: &Vec<Vec<char>>,
-    target_row: usize,
-    target_col: usize,
-    dir_row: isize,
-    dir_col: isize,
-    target_char: char,
-    accum: &mut isize,
-) {
-    let current_char = map[target_row][target_col];
-    // println!("Current character is {} at row {}, column {}",
-    //     current_char,
-    //     target_row + 1,
-    //     target_col + 1);
-    if target_char == current_char {
-        // println!(
-        //     "\'{}\' at row {}, column {}",
-        //     target_char,
-        //     target_row + 1,
-        //     target_col + 1
-        // );
-        match target_char {
-            'M' => {
-                if target_row as isize + (dir_row as isize) >= 0
-                    && target_row as isize + (dir_row as isize) < map.len() as isize
-                    && target_col as isize + (dir_col as isize) >= 0
-                    && target_col as isize + (dir_col as isize) < map[target_row].len() as isize
-                {
-                    search_directional(
-                        map,
-                        target_row.saturating_add_signed(dir_row),
-                        target_col.saturating_add_signed(dir_col),
-                        dir_row,
-                        dir_col,
-                        'A',
-                        accum,
-                    );
-                }
-            }
-
-            'A' => {
-                if target_row as isize + (dir_row as isize) >= 0
-                    && target_row as isize + (dir_row as isize) < map.len() as isize
-                    && target_col as isize + (dir_col as isize) >= 0
-                    && target_col as isize + (dir_col as isize) < map[target_row].len() as isize
-                {
-                    search_directional(
-                        map,
-                        target_row.saturating_add_signed(dir_row),
-                        target_col.saturating_add_signed(dir_col),
-                        dir_row,
-                        dir_col,
-                        'S',
-                        accum,
-                    );
-                }
-            }
-
-            'S' => {
-                accum.add_assign(1);
-            }
-
-            _ => {}
-        }
-    }
-}
-
-#[allow(unused_variables)]
 fn part1(input: &str) -> isize {
-    let map = input
-        .lines()
-        .map(|l| l.chars().collect::<Vec<char>>())
-        .collect::<Vec<Vec<char>>>();
-    let mut count: isize = 0;
-
-    for row in 0..map.len() {
-        for column in 0..map[row].len() {
-            if map[row][column] == 'X' {
-                // println!("Starting search at row {}, column {}", row + 1, column + 1);
-                // println!("Starting at \'X\' at row {}, column {}", row + 1, column + 1);
-                search_all_dir(&map, row, column, &mut count);
-            }
-        }
-    }
-
-    return count;
+    let re = RegexBuilder::new(r"mul\((?<n1>\d+),(?<n2>\d+)\)")
+        .build()
+        .unwrap();
+    re.captures_iter(input).fold(0, |a, c| {
+        a + c["n1"].parse::<isize>().unwrap() * c["n2"].parse::<isize>().unwrap()
+    })
 }
 
-#[allow(unused_variables)]
 fn part2(input: &str) -> isize {
-    todo!()
+    let re = RegexBuilder::new(r"(?:mul\((?<n1>\d+),(?<n2>\d+)\))|(?<dont>don't)|(?<do>do)")
+        .build()
+        .unwrap();
+    let mut mul = true;
+
+    re.captures_iter(input).fold(0, |a, c| {
+        if let Some(_) = c.name("do") {
+            mul = true;
+            a
+        } else if let Some(_) = c.name("dont") {
+            mul = false;
+            a
+        } else if mul {
+            a + c["n1"].parse::<isize>().unwrap() * c["n2"].parse::<isize>().unwrap()
+        } else {
+            a
+        }
+    })
 }
