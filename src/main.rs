92c9fcc5af2767350a576be3e31238e161eb2ab4
TheRealSleeper <140853156+TheRealSleeper@users.noreply.github.com>
2024-12-20 15:07:19 -0600

    both parts solved

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,7 @@ fn part1(input: &str) -> isize {
 
     let mut left: Vec<u32> = vec![];
     let mut right: Vec<u32> = vec![];
-    for (l, r) in (raw_nums) {
+    for (l, r) in raw_nums {
         left.push(l);
         right.push(r);
     }
@@ -62,5 +62,26 @@ fn part1(input: &str) -> isize {
 }
 
 fn part2(input: &str) -> isize {
-    todo!()
+    let raw_nums = input
+        .lines()
+        .map(|l| {
+            let p = l.split_once("   ").unwrap();
+            (p.0.parse::<u32>().unwrap(), p.1.parse::<u32>().unwrap())
+        })
+        .collect::<Vec<(u32, u32)>>();
+
+    let mut left: Vec<u32> = vec![];
+    let mut right: Vec<u32> = vec![];
+    for (l, r) in raw_nums {
+        left.push(l);
+        right.push(r);
+    }
+
+    left.sort();
+    right.sort();
+
+    left.iter()
+        .map(|n| {
+            n * right.iter().filter(|f| *f == n).count() as u32
+        }).sum::<u32>() as isize
 }
