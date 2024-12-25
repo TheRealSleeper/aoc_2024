838e1e2a692397f812fd8423bd0fd27b2e301988
TheRealSleeper <asleeper135@gmail.com>
2024-12-25 13:40:52 -0600

    solved part 2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::collections::HashSet;
 use std::fs::read_to_string;
+use std::sync::Arc;
 
 mod aoc_utils;
 mod tests;
@@ -44,12 +46,23 @@ enum Space {
     Guard(Direction),
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 enum Direction {
     Up,
     Down,
     Left,
     Right,
+}
+
+impl std::fmt::Display for Direction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Direction::Up => write!(f, "Up"),
+            Direction::Down => write!(f, "Down"),
+            Direction::Left => write!(f, "Left"),
+            Direction::Right => write!(f, "Right"),
+        }
+    }
 }
 
 fn part1(input: &str) -> isize {
@@ -131,9 +144,167 @@ fn part1(input: &str) -> isize {
         }
     }
 
-    map.iter().map(|l|l.iter().filter(|s| *s == &Space::Visited).count()).sum::<usize>() as isize
+    map.iter()
+        .map(|l| l.iter().filter(|s| *s == &Space::Visited).count())
+        .sum::<usize>() as isize
 }
 
 fn part2(input: &str) -> isize {
-    todo!()
+    let mut guard_position = (0, 0);
+    let mut guard_direction = Direction::Up;
+    let map = input
+        .lines()
+        .map(|l| {
+            l.chars()
+                .map(|c| match c {
+                    '#' => Space::Obstacle,
+                    '^' => Space::Guard(Direction::Up),
+                    'v' => Space::Guard(Direction::Down),
+                    '>' => Space::Guard(Direction::Right),
+                    '<' => Space::Guard(Direction::Left),
+                    _ => Space::Empty,
+                })
+                .collect::<Vec<Space>>()
+        })
+        .collect::<Vec<Vec<Space>>>();
+
+    for (i, row) in map.iter().enumerate() {
+        for (ii, space) in row.iter().enumerate() {
+            if let Space::Guard(dir) = *space {
+                guard_position = (i as i32, ii as i32);
+                guard_direction = dir;
+            }
+        }
+    }
+
+    let starting_position = guard_position.clone();
+    let starting_direction = guard_direction;
+
+    let map: Arc<Vec<Vec<Space>>> = Arc::from(map);
+    let mut functions_collection = vec![];
+    for _ in 0..std::thread::available_parallelism().unwrap().into() {
+        functions_collection.push(vec![]);
+    }
+    let mut i = (0..functions_collection.len()).cycle();
+
+    for r in 0..map.len() {
+        for c in 0..map[r].len() {
+            if (r as i32, c as i32) == starting_position {
+                continue
+            }
+            
+            let m = map.clone();
+            let f = move || -> bool {
+                check_new_obstacle(
+                    starting_position.clone(),
+                    starting_direction,
+                    m,
+                    (r as i32, c as i32),
+                )
+            };
+            
+            functions_collection[i.next().unwrap()].push(f);
+        }
+    }
+
+    let mut handles = vec![];
+    while let Some(functions) = functions_collection.pop() {
+        handles.push(std::thread::spawn(move || {
+            let mut total = 0;
+            for function in functions {
+                if function() {
+                    total += 1;
+                }
+            }
+            total
+        }))
+    }
+    let mut results = vec![];
+    for handle in handles {
+        results.push(handle.join().unwrap());
+    }
+
+    results.iter().sum()
+}
+
+fn check_new_obstacle(
+    guard_position: (i32, i32),
+    guard_direction: Direction,
+    map: Arc<Vec<Vec<Space>>>,
+    obstacle_position: (i32, i32),
+) -> bool {
+    let mut guard_position = guard_position;
+    let mut guard_direction = guard_direction;
+
+    let mut hash = HashSet::new();
+    hash.insert((guard_position, guard_direction));
+
+    loop {
+
+        match guard_direction {
+            Direction::Up => {
+                if guard_position.0 <= 0 {
+                    break;
+                } else if map[guard_position.0 as usize - 1][guard_position.1 as usize]
+                    == Space::Obstacle
+                    || (guard_position.0 - 1, guard_position.1) == obstacle_position
+                {
+                    guard_direction = Direction::Right;
+                } else {
+                    guard_position.0 -= 1;
+                }
+            }
+            Direction::Down => {
+                if guard_position.0 >= map.len() as i32 - 1 {
+                    break;
+                } else if map[guard_position.0 as usize + 1][guard_position.1 as usize]
+                    == Space::Obstacle
+                    || (guard_position.0 + 1, guard_position.1) == obstacle_position
+                {
+                    guard_direction = Direction::Left;
+                } else {
+                    guard_position.0 += 1;
+                }
+            }
+            Direction::Left => {
+                if guard_position.1 <= 0 {
+                    break;
+                } else if map[guard_position.0 as usize][guard_position.1 as usize - 1]
+                    == Space::Obstacle
+                    || (guard_position.0, guard_position.1 - 1) == obstacle_position
+                {
+                    guard_direction = Direction::Up;
+                } else {
+                    guard_position.1 -= 1;
+                }
+            }
+            Direction::Right => {
+                if guard_position.1 >= map[guard_position.0 as usize].len() as i32 - 1 {
+                    break;
+                } else if map[guard_position.0 as usize][guard_position.1 as usize + 1]
+                    == Space::Obstacle
+                    || (guard_position.0, guard_position.1 + 1) == obstacle_position
+                {
+                    guard_direction = Direction::Down;
+                } else {
+                    guard_position.1 += 1;
+                }
+            }
+        }
+
+        if hash.contains(&(guard_position, guard_direction)) {
+            // println!(
+            //     "Object at {}, {}, from guard starting {}, {} facing {}",
+            //     obstacle_position.0 + 1,
+            //     obstacle_position.1 + 1,
+            //     starting_position.0 + 1,
+            //     starting_position.1 + 1,
+            //     starting_direction
+            // );
+            return true;
+        }
+        hash.insert((guard_position, guard_direction));
+    }
+
+    false
 }
--- src/tests.rs
+++ src/tests.rs
@@ -7,7 +7,13 @@ fn test1() {
 
 #[test]
 fn test2() {
-    const ANSWER2: isize = 0;
+    const ANSWER2: isize = 6;
     let sample2 = std::fs::read_to_string("sample2.txt").expect("Unable to read file");
     assert_eq!(super::part2(&sample2), ANSWER2);
+}
+
+#[test]
+fn test2_additional() {
+    let additional = std::fs::read_to_string("part2_addition.txt").unwrap();
+    assert_eq!(super::part2(&additional), 1);
 }
