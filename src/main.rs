23c570fff23daf561e535ffd1c15f2827d10b499
TheRealSleeper <asleeper135@gmail.com>
2024-12-25 17:25:55 -0600

    both parts solved

--- src/main.rs
+++ src/main.rs
@@ -36,12 +36,68 @@ fn main() {
     }
 }
 
-
 fn part1(input: &str) -> isize {
-    todo!()
+    input
+        .lines()
+        .filter_map(|l| {
+            let (val, nums) = l.split_once(':').unwrap();
+            let val = val.parse::<u64>().unwrap();
+            let mut nums = nums
+                .split_whitespace()
+                .map(|s| s.parse::<u64>().unwrap())
+                .collect::<Vec<u64>>();
+            nums.reverse();
+            let first = nums.pop().unwrap();
+            if is_valid(val, first, nums) {
+                Some(val)
+            } else {
+                None
+            }
+        })
+        .sum::<u64>() as isize
 }
 
-
 fn part2(input: &str) -> isize {
-    todo!()
+    input
+        .lines()
+        .filter_map(|l| {
+            let (val, nums) = l.split_once(':').unwrap();
+            let val = val.parse::<u64>().unwrap();
+            let mut nums = nums
+                .split_whitespace()
+                .map(|s| s.parse::<u64>().unwrap())
+                .collect::<Vec<u64>>();
+            nums.reverse();
+            let first = nums.pop().unwrap();
+            if is_valid_concat(val, first, nums) {
+                Some(val)
+            } else {
+                None
+            }
+        })
+        .sum::<u64>() as isize
+}
+
+fn is_valid(val: u64, current: u64, mut nums: Vec<u64>) -> bool {
+    if let Some(n) = nums.pop() {
+        is_valid(val, current + n, nums.clone()) || is_valid(val, current * n, nums)
+    } else {
+        current == val
+    }
+}
+
+fn is_valid_concat(val: u64, current: u64, mut nums: Vec<u64>) -> bool {
+    if let Some(n) = nums.pop() {
+        is_valid_concat(val, current + n, nums.clone())
+            || is_valid_concat(val, current * n, nums.clone())
+            || is_valid_concat(
+                val,
+                format!("{}{}", current.to_string(), n.to_string())
+                    .parse()
+                    .unwrap(),
+                nums,
+            )
+    } else {
+        current == val
+    }
 }
